0ee2416e5cf71dbe329f32d48a2aa12e881ff738
ana caroline <agatalinda2027@gmail.com>
2025-05-07 01:20:44 -0400

    corrigir o erro do da classe estrutura for

--- estruturas-de-repeticoes/src/EstruturaFor.java
+++ estruturas-de-repeticoes/src/EstruturaFor.java
@@ -1,8 +1,5 @@
 public class EstruturaFor {
-
-    public static void main(String[] args){
-         for (int carro = 1; carro <=20; carro ++ ){
-             System.out.println("Contando Carro " + carro + " está na pista.");
-        }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
     }
-    }
+}
