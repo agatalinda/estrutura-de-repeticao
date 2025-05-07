5645e6fe0dc4f1be3cfc3dd54ba396340fb3dc53
ana caroline <agatalinda2027@gmail.com>
2025-05-07 01:32:32 -0400

    criei outra classe Estrutura For Arrays

--- estruturas-de-repeticoes/src/EstruturaFor.java
+++ estruturas-de-repeticoes/src/EstruturaFor.java
@@ -1,5 +1,10 @@
 public class EstruturaFor {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        for (int carro =1; carro <= 20; carro ++){
+            System.out.println("Contando os carros "  +carro+  "º na pista");
+        }
+        System.out.println("Todos os carros passaram na pista!");
+        System.out.println("A corrida acabou!");
+       
     }
 }
