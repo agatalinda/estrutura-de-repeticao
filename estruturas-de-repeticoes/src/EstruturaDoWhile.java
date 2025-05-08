5439a8377593b5eb287e6b53a0abde3e1f00775d
ana caroline <agatalinda2027@gmail.com>
2025-05-08 14:20:09 -0400

    corrigindo o codigo

--- estruturas-de-repeticoes/src/EstruturaDoWhile.java
+++ estruturas-de-repeticoes/src/EstruturaDoWhile.java
@@ -1,13 +1,17 @@
 import java.util.Random;
 public class EstruturaDoWhile {
-public static void main(String[] args) {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         System.out.println("Discando...");
         do{
             //excutando repetidas vezes até alguém atenter
             System.out.println("Telefone tocando...");
         }while(tocando());
+
+        System.out.println("Alô!!!");
     }
-}
-    
+    private static boolean tocando(){
+        boolean atendeu = new Random().nextInt(3)==1; 
+        System.out.println("Alguém atendeu? " + atendeu);
+        return !atendeu;
+    }
 }
