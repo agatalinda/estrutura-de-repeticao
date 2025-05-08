f1993980ec250982007a316ff413dec8b6695b40
ana caroline <agatalinda2027@gmail.com>
2025-05-08 06:48:21 -0400

    foi colocado o codigo na classe estrutura for array

--- estruturas-de-repeticoes/src/EstruturaForArrays.java
+++ estruturas-de-repeticoes/src/EstruturaForArrays.java
@@ -1,3 +1,9 @@
 public class EstruturaForArrays {
-    
+  public static void main(String[] args) {
+    String alunos[] ={"Lucas", "Ana", "Pedro", "Maria", "João"};
+
+    for (int x=0; x<alunos.length; x++){
+      System.out.println("O aluno no indice x=  " +x + "º é " +alunos[x]    );
+    }
+  }  
 }
