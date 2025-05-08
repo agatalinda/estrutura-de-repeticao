d3a08813f570fcb89d06d09e61902538f7a8956c
ana caroline <agatalinda2027@gmail.com>
2025-05-08 06:49:03 -0400

    Foi criado a classe estrutura break continue

---
+++ estruturas-de-repeticoes/src/EstruturaBreakContinue.java
@@ -0,0 +1,11 @@
+
+public class EstruturaBreakContinue {
+  public static void main(String[] args) {
+    for(int numero =1; numero <=5; numero ++){
+        if (numero ==3)
+        break; // interrompe o loop quando numero for igual a 3 para continuar colocar continue que irá pular o número 3 e incerrar no 5.
+        System.out.println("O número é " + numero);
+    }
+   
+    }
+  }  
