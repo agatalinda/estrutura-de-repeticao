d0d8f7b5518af9085505d39016c224e5a1638c5a
ana caroline <agatalinda2027@gmail.com>
2025-05-08 14:01:10 -0400

    foi criado a clase estrutura do while e adicionado o codigo

---
+++ estruturas-de-repeticoes/src/EstruturaDoWhile.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+public class EstruturaDoWhile {
+public static void main(String[] args) {
+    public static void main(String [] args){
+        System.out.println("Discando...");
+        do{
+            //excutando repetidas vezes até alguém atenter
+            System.out.println("Telefone tocando...");
+        }while(tocando());
+    }
+}
+    
+}
